0633701eb5b823d4fd141ba7fad19abc22256237
vyomeshsathya <vyomeshsathya@gmail.com>
2026-09-19 01:25:16 -0400

    Complete Stage 1 road network foundation

--- app/map/inspect_network.py
+++ app/map/inspect_network.py
@@ -8,7 +8,7 @@ print("Load complete.")
 print(f"Nodes: {len(graph.nodes)}")
 print(f"Edges: {len(graph.edges)}")
 
-# show one real road-nework node and one real rode segment including the info attached to them
+# show one real road-network node and one real rode segment including the info attached to them
 node_id, node_data = next(iter(graph.nodes(data=True)))
 
 print("\nExample node:")
@@ -21,3 +21,21 @@ print("\nExample edge:")
 print("From:", u)
 print("To:", v)
 print("Data:", edge_data)
+
+print("\nPlotting road network...")
+
+fig, ax = ox.plot_graph(
+    graph,
+    node_size = 0,
+    edge_linewidth = 0.8,
+    show = False,
+    close = False
+)
+
+fig.savefig(
+    "data/raw/waterloo_road_network.png",
+    dpi = 200,
+    bbox_inches = 'tight'
+)
+
+print("Map image saved.")
